d3f7f632b48662fb2f230c2c70bcc4e261fbb746
vaishnav-mv <vaishnavam02@gmail.com>
2025-04-19 23:43:37 +0530

    bring categories inside menu. created accordion to display and hide items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import Shimmer from "./Shimmer"
 import { useParams } from "react-router-dom"
 import useRestaurantMenu from "../utils/useRestaurantMenu"
+import RestaurantCategory from "./RestaurantCategory"
 
 const RestaurantMenu = () => {
 
@@ -8,25 +9,20 @@ const RestaurantMenu = () => {
 
     const resInfo = useRestaurantMenu(resId)
 
-
     if (resInfo === null) return <Shimmer></Shimmer>
 
     const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info || {}
 
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
-    console.log(itemCards);
-
+    //console.log(itemCards)
+    const categories=resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=>c?.card?.card?.['@type']==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+    //console.log(categories)
 
     return (
-        <div className="menu">
-            <h1>{name}</h1>
-            <h2>{cuisines.join(",")}</h2>
-            <h2>{costForTwoMessage}</h2>
-            <h2>Menu</h2>
-            <ul>
-                {itemCards.map(item => <li key={item.card.info.id}>{item.card.info.name}-{"Rs."}{item.card.info.price / 100}</li>)}
-            </ul>
-
+        <div className="text-center">
+            <h1 className="font-bold my-5 text-2xl ">{name}</h1>
+            <h2 className="font-bold text-lg">{cuisines.join(",")}</h2>
+            {categories.map((category)=><RestaurantCategory data={category.card.card} key={category.card.card.categoryId} ></RestaurantCategory>)}
         </div>
     )
 }
