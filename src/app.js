b96077c2a90a059d90ee259ec1533f9996f461d0
vaishnav-mv <vaishnavam02@gmail.com>
2025-04-22 09:49:28 +0530

    modifying context data using UserContext.Provider

--- src/app.js
+++ src/app.js
@@ -1,52 +1,70 @@
-import React,{lazy,Suspense} from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
-import { createBrowserRouter,RouterProvider,Outlet} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import UserContext from "./utils/UserContext";
 // import Grocery from "./components/Grocery";
 
-const Grocery=lazy(()=>import("./components/Grocery"))
+const Grocery = lazy(() => import("./components/Grocery"))
 
 const AppLayout = () => {
-    return <div className="app">
-        <Header></Header>
-        <Outlet></Outlet>
-    </div>
+    const [userName, setUserName] = useState()
+    //authentication
+    useEffect(() => {
+        //make an api call to send username and password
+        const data = {
+            name: "Akshay Saini"
+        }
+        setUserName(data.name)
+    }, [])
+
+    return (
+        <UserContext.Provider value={{loggedInUser:userName}}>
+            <div className="app">
+                <UserContext.Provider value={{loggedInUser:"vaishnav"}}>
+                    <Header></Header>
+                </UserContext.Provider>
+                <Outlet></Outlet>
+            </div>
+        </UserContext.Provider>
+    )
+
 }
 
-const appRouter=createBrowserRouter([
+const appRouter = createBrowserRouter([
     {
-        path:"/",
-        element:<AppLayout></AppLayout>,
-        children:[
+        path: "/",
+        element: <AppLayout></AppLayout>,
+        children: [
             {
-                path:"/",
-                element:<Body></Body>
+                path: "/",
+                element: <Body></Body>
             },
             {
-                path:"/about",
-                element:<About></About>
+                path: "/about",
+                element: <About></About>
             },
             {
-                path:"/contact",
-                element:<Contact></Contact>
+                path: "/contact",
+                element: <Contact></Contact>
             },
             {
-                path:"/grocery",
-                element:<Suspense fallback={<h1>Loading....</h1>}><Grocery></Grocery></Suspense>
+                path: "/grocery",
+                element: <Suspense fallback={<h1>Loading....</h1>}><Grocery></Grocery></Suspense>
             },
             {
-                path:"/restaurants/:resId",
-                element:<RestaurantMenu></RestaurantMenu>
+                path: "/restaurants/:resId",
+                element: <RestaurantMenu></RestaurantMenu>
             }
         ],
-        errorElement:<Error></Error>
+        errorElement: <Error></Error>
     },
-    
+
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
